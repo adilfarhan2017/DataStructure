f0e7cff2ab2c577e264eb2981a3c17589a256503
Adil <adil.farhan@gmail.com>
2020-05-03 12:50:35 +0530

    foundational classes

---
+++ src/main/java/com/practise/mainprograms/FactorialMainProgram.java
@@ -0,0 +1,18 @@
+package com.practise.mainprograms;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FactorialMainProgram {
+
+    public static Logger logger = LoggerFactory.getLogger(FactorialMainProgram.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Hello world");
+
+
+
+    }
+}
